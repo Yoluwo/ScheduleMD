16433c5d86ce124ffdd21d4be11f52e07f36ab5d
585594 <585594@ELW9IT223067.ACDM.DS.SAIT.CA>
2021-06-08 08:41:08 -0600

    Added database codebase to AccountService Errors still need to be rectified And sql script has not been added to the database

--- src/java/services/AccountService.java
+++ src/java/services/AccountService.java
@@ -5,12 +5,67 @@
  */
 package services;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.User;
 /**
  *
  * @author epaul
  */
 public class AccountService {
+    
+    private String dbUrl = "jdbc:mysql://localhost:3306/userdb";
+    private String dbUname = "root";
+    private String dbPassword = "rootpasswordgiven";
+    private String dbDriver = "com.mysql.cj.jdbc.Driver";
+    
+    public void loadDriver(String dbDriver) {
+        try {
+            Class.forName(dbDriver);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LoginDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public Connection getConnection() {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(dbUrl, dbUname, dbPassword);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
+
+    public boolean validate(LoginBean loginBean) {
+        loadDriver(dbDriver);
+        Connection con = getConnection();
+        boolean status = false;
+        
+        String sql = "Select * from login where username = ? and password = ?";
+        
+        PreparedStatement ps;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, loginBean.getUsername());
+            ps.setString(2, loginBean.getPassword());
+            
+        ResultSet rs = ps.executeQuery();
+        status = rs.next();
+        
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+                
+        return status;
+        
+    }
+    
     
     public User login(String email, String password) {
         //UserDB userDB = new UserDB();
