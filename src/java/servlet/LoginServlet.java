eb1e88d503ead0bdf9226dbfaf7612659a5445b3
743851 <743851@SAIT225212IT.ACDM.DS.SAIT.CA>
2021-06-10 17:49:50 -0600

    Added logic to login servlet

--- src/java/servlet/LoginServlet.java
+++ src/java/servlet/LoginServlet.java
@@ -10,38 +10,42 @@ import services.AccountServiceWrong;
 
 public class LoginServlet extends HttpServlet {
 
-     @Override
-     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-             throws ServletException, IOException {
-          getServletContext().getRequestDispatcher("/WEB-INF/login.jsp")
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/login.jsp")
                 .forward(request, response);
-          }
+    }
 
-     @Override
-     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-             throws ServletException, IOException {
-           getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
-          
-           HttpSession session = request.getSession();
-           AccountServiceWrong accountService = new AccountServiceWrong();
-           
-           String email = request.getParameter("email");
-           String password = request.getParameter("password");
-           
-           User currentUser = accountService.login(email, password);
-           
-            if (currentUser == null) {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+
+        HttpSession session = request.getSession();
+        AccountServiceWrong accountService = new AccountServiceWrong();
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        User currentUser = accountService.login(email, password);
+
+        if (currentUser == null) {
             request.setAttribute("message", "Your account does not exist");
             getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
 
             return;
             
-            
         }
-           
-           
-            
-            
-     }
- 
+
+        if (currentUser.getRoleId() == 0)  {
+              response.sendRedirect("admin");
+        } else {
+              response.sendRedirect("resident");
+            }
+
+        }
+
+    }
+
 }
