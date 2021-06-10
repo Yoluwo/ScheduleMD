91c19979c5aa15fefd8244ec8cec7ebda1d94024
epaul <epaul@DESKTOP-69SK426>
2021-06-10 17:26:27 -0600

    Ethan - Updated account service Class

--- src/java/services/AccountService.java
+++ src/java/services/AccountService.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package services;
+import model.User;
 
 /**
  *
@@ -11,4 +12,23 @@ package services;
  */
 public class AccountService {
     
+    public User login(String email, String password) {
+
+        UserDB userDB = new UserDB();
+        User user = null;
+        try {
+            user = userDB.get(email);
+            if (password.equals(user.getPassword()) && user.getActive()) {
+                return user;
+            } else {
+                user = null;
+            }
+        } catch (Exception e) {
+        }
+
+        return user;
+    }
+    public void changePassword(String email, String oldPassword, String newPassword) {
+
+    }
 }
