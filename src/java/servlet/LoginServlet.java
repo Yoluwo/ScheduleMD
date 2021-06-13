c6a6276e85066290f6f4a73c5c779300d6fd5052
743851 <743851@SAIT225212IT.ACDM.DS.SAIT.CA>
2021-06-13 15:16:09 -0600

    logic for servlets

--- src/java/servlet/LoginServlet.java
+++ src/java/servlet/LoginServlet.java
@@ -15,7 +15,15 @@ public class LoginServlet extends HttpServlet {
   @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
         HttpSession session = request.getSession();
+        
+          if (request.getParameter("logout") != null) {
+            session.invalidate();
+            request.setAttribute("message", "Succesfully Logged Out");
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        }
+          
         session.invalidate();
         getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
