6489701d3eecee7878fcc1eb81594283cdb52444
epaul <epaul@DESKTOP-69SK426>
2021-06-13 15:45:46 -0600

    Ethan - Updated UserDB class

--- src/java/services/AccountService.java
+++ src/java/services/AccountService.java
@@ -30,7 +30,38 @@ public class AccountService {
         return user;
 */
     }
-    public void changePassword(String email, String oldPassword, String newPassword) {
-
+    public boolean changePassword(String email, String oldPassword, String newPassword) {
+        //Passwords requirements -- 1 uppercase, 1 lowercase, 1 number, minimum 8 length
+        boolean hasUpperCase = false;
+        boolean hasLowerCase = false;
+        boolean hasNumber = false;
+        char ch;
+        UserDB userDB = new UserDB();
+        User user = null;
+        try {
+            user = userDB.get(email);
+            if(!oldPassword.equals(newPassword)){
+                if(newPassword.length() >= 8){
+                    for(int i = 0; i < newPassword.length(); i++){
+                        ch = newPassword.charAt(i);
+                        if(Character.isDigit(ch)){
+                            hasNumber = true;
+                        }
+                        else if(Character.isUpperCase(ch)){
+                            hasUpperCase = true;
+                        }
+                        else if(Character.isLowerCase(ch)){
+                            hasLowerCase = true;
+                        }
+                    }
+                    if(hasNumber && hasUpperCase && hasLowerCase){
+                        user.setPassword(newPassword);
+                        userDB.update(user);
+                        return true;
+                    }
+                }
+            }
+        } catch(Exception e) {}
+        return false;
     }
 }
