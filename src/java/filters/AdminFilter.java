38d7ad222fb3018ca7c98329536aebf0dab965a4
Alex Zecevic <alexzecevic15@gmail.com>
2021-06-13 15:29:24 -0600

    Filters. - Added logic for filters.

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -1,5 +1,6 @@
 package filters;
 
+import dataaccess.UserDB;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -7,13 +8,30 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AdminFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain)
             throws IOException, ServletException {
-        
+        try {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            HttpSession session = httpRequest.getSession();
+            String email = (String)session.getAttribute("email");
+            UserDB userDB = new UserDB();
+            int role = userDB.get(email).getRoleID().getRoleID();
+
+            if (role != 0) {
+                HttpServletResponse httpResponse = (HttpServletResponse) response;
+                httpResponse.sendRedirect("resident");
+                return;
+            }
+
+            chain.doFilter(request, response);
+        } catch (Exception ex) {}
     }   
 
     @Override
--- src/java/filters/AuthenticationFilter.java
+++ src/java/filters/AuthenticationFilter.java
@@ -7,13 +7,26 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AuthenticationFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain)
             throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+        HttpSession session = httpRequest.getSession();
+        String email = (String)session.getAttribute("email");
 
+        if (email == null) {
+            HttpServletResponse httpResponse = (HttpServletResponse)response;
+            httpResponse.sendRedirect("login");
+            return;
+        }
+
+        chain.doFilter(request, response);
     }
 
     @Override
