834e3a0c8261d80e38f67f66d495b076d6e25f6d
585594 <585594@ELW9IT223067.ACDM.DS.SAIT.CA>
2021-06-22 08:51:37 -0600

    getHospital method done

--- src/java/services/SchedulingService.java
+++ src/java/services/SchedulingService.java
@@ -5,8 +5,9 @@
  */
 package services;
 
+import dataaccess.DBUtil;
+import javax.persistence.EntityManager;
 import models.Hospital;
-import static models.Schedule_.hospital;
 import org.eclipse.persistence.jpa.jpql.parser.DateTime;
 
 
@@ -17,7 +18,7 @@ import org.eclipse.persistence.jpa.jpql.parser.DateTime;
 
 /*
     The SchedulingService class is used to perform queries to the database 
-    for data on residents, shifts, hospitals and status of requests.
+    for data on all residents, shifts, hospitals and status of requests.
 */
 
 
@@ -34,12 +35,15 @@ public class SchedulingService {
     
     public getAllActiveResdients() {
         
-    }
+    } **/
     
-    public Hospital getHospital(Integer hospitalID) {
+    public static Hospital getHospital(Integer hospitalID) {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        Hospital hospital;
+        hospital = em.find(Hospital.class, hospitalID);
         return hospital;
     }
-    
+    /**
     public Shift getShift(DateTime startDate, Integer hospitalID) {
         return Shift();
     }
