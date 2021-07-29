d8aa2bb4cd13bbe4efa610c60df6845644958dbd
Akash <Akash@AP>
2021-07-29 01:15:34 -0600

    updated profile page

--- src/java/servlets/ProfileServlet.java
+++ src/java/servlets/ProfileServlet.java
@@ -1,11 +1,15 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import models.Role;
+import models.User;
 import services.AccountService;
 
 public class ProfileServlet extends HttpServlet {
@@ -13,6 +17,31 @@ public class ProfileServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        AccountService accountService = new AccountService();
+        String email = (String) session.getAttribute("email");
+        User user;
+        try {
+            user = accountService.getUser(email);
+            String firstName = user.getFirstName();
+            String lastName = user.getLastName();
+            String userEmail = user.getEmail();
+            Role role = user.getRole();
+            int roleId = role.getRoleID();
+            String roleName = role.getRoleName();
+            System.out.println(role.getRoleID());
+            System.out.println(role.getRoleName());
+            
+//            System.out.println(user.getFirstName());
+            request.setAttribute("firstName", firstName);
+            request.setAttribute("lastName", lastName);
+            request.setAttribute("email", userEmail);
+            request.setAttribute("role", roleId );
+            request.setAttribute("rolename", roleName); 
+        } catch (Exception ex) {
+            Logger.getLogger(ProfileServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         getServletContext().getRequestDispatcher("/WEB-INF/profile.jsp")
                 .forward(request, response);
     }
